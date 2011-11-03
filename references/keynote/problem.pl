d76b2c9ef1c941b6918012ebfafd415ae897da80
ynonp <ynonperek@gmail.com>
2011-11-03 17:56:46 +0200

    bad solution

--- references/keynote/problem.pl
+++ references/keynote/problem.pl
@@ -6,3 +6,14 @@ sub longer {
     my (@l1, @l2) = @_;
     return @l1 > @l2 ? @l1 : @l2;
 }
+
+sub longer_better {
+    my $sz1 = shift;
+    my @l1  = splice(@_, 0, $sz1);
+    my $sz2 = shift;
+    my @l2  = splice(@_, 0, $sz2);
+
+    return @l1 > @l2 ? @l1 : @l2;
+}
+
+print longer_better(2, (2, 3), 3, (9, 8, 7));
