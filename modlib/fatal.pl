5113ca3b64400fc38b0b9c417e50bce9eb273458
ynonp <ynonperek@gmail.com>
2011-11-06 20:03:19 +0200

    core

---
+++ modlib/fatal.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use Fatal qw/open close/;
+
+sub count_lines {
+    my ($filename) = @_;
+    my $lines = 0;
+
+    open my $f, '<', $filename;
+    $lines++ while (<$f>);
+    close $f;
+
+    return $lines;
+}
+
+print count_lines(@ARGV), "\n";
