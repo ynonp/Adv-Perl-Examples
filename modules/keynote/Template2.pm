77d0e71647ad26885fb8260dd3816537dc4811e5
ynonp <ynonperek@gmail.com>
2011-12-03 01:44:44 +0200

    modules

---
+++ modules/keynote/Template2.pm
@@ -0,0 +1,23 @@
+package ModuleName;
+use strict;
+use warnings;
+
+1;
+
+=head1 NAME
+
+ModuleName is a cool module to do the job
+
+=head1 DESCRIPTION
+
+=over
+
+=item func1(x, y)
+
+=item func2(x, y, z)
+
+=back
+
+=head1 SEE ALSO
+
+=head1 BUGS
