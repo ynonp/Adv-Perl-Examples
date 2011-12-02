809d928839e12bb7e1485df12881b36585058bf6
ynonp <ynonperek@gmail.com>
2011-12-03 01:21:01 +0200

    modules

---
+++ modules/keynote/AliensPkg.pm
@@ -0,0 +1,13 @@
+package Aliens;
+
+use strict;
+
+sub secret_weapon {
+    print("Aliens win\n")
+}
+
+sub hit_plants {
+    secret_weapon();
+}
+
+1;
---
+++ modules/keynote/match2.pl
@@ -0,0 +1,5 @@
+use strict;
+use warnings;
+
+use AliensPkg;
+Aliens::hit_plants();
