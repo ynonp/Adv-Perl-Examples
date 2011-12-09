b793b9880a560094e88ad64b447fe69f8f528ca2
ynonp <ynonperek@gmail.com>
2011-12-09 12:26:58 +0200

    oop examples

---
+++ oop/keynote/subtypes.pl
@@ -0,0 +1,19 @@
+package Person;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+subtype 'age',
+    as 'Int',
+    where { $_ >= 0 && $_ <= 120 },
+    message { "Invalid Age: $_ "};
+
+has 'age', is => 'rw', isa => 'age', default => 0;
+
+package main;
+my $p = Person->new;
+
+# works
+$p->age(10);
+
+# fail
+$p->age(140);
