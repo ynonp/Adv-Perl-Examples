6c78368448c9ea7c663f08667089973b5900f4df
ynonp <ynonperek@gmail.com>
2011-12-09 10:54:21 +0200

    oop examples

---
+++ oop/keynote/rw.pl
@@ -0,0 +1,15 @@
+package Product;
+use Moose;
+
+has 'name' => (
+    is     => 'rw',
+    reader => 'get_name',
+    writer => '_set_name',
+);
+
+has 'price' => (
+    is     => 'rw',
+    reader => 'get_price',
+    writer => 'set_price',
+);
+
