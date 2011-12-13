92e5c2771df186fcbc9070b1299a0870a63f8878
ynonp <ynonperek@gmail.com>
2011-12-13 10:11:02 +0200

    editor solution

---
+++ modules/tk/editor.pl
@@ -0,0 +1,35 @@
+use strict;
+use warnings;
+use Tk;
+use File::Slurp;
+
+sub load_file {
+    my ($editor, $filename) = @_;
+    my $content = read_file($filename);
+    $editor->Insert($content);
+}
+
+sub save_file {
+    my ($editor, $filename) = @_;
+
+    return sub {
+        my $content = $editor->get('1.0', 'end');
+        write_file($filename, $content);
+    }
+}
+
+my $USAGE = <<"END";
+    Usage: editor.pl <filename>
+END
+
+my $filename = shift or die $USAGE;
+my $w = MainWindow->new;
+my $t = $w->Text->pack;
+my $save_btn = $w->Button(
+    -text => 'Save',
+    -command => save_file($t, $filename),
+)->pack(-expand => 'true', -fill => 'both');
+
+load_file($t, $filename);
+
+MainLoop;
